4fbd32ac7e5d425318caa9137cda76cf10adabfc
Benedict <newlyf38@gmail.com>
2023-09-03 16:36:20 +0000

    created a react project, class and functional component and imported them

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class Welcome extends Component{
     render(){
         return (
-            <h1>Class Component</h1>
+            <h1>Class Component is greate</h1>
         )
     }
 }
