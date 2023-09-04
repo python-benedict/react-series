449b7b03513385c9d018d8abb3512222dcd64e93
Benedict <newlyf38@gmail.com>
2023-09-04 01:58:36 +0000

    added state to the project

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
+      <Message />
       <Greet name="Jikla" heroName="Steve Jobs"/>
       <Welcome name="Benedict" heroName="Linux"/>
     </div>
---
+++ src/components/Message.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+
+class Message extends Component{
+    constructor(){
+        super()
+        this.state = {
+            message: "Please subscribe."
+        }
+    }
+
+    changeMessage(){
+        this.setState({
+            message: "Thank you for subscribing"
+        })
+    }
+    
+    render(){
+        return(
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={()=>{this.changeMessage()}}>Click</button>
+            </div>
+        )
+    }
+}
+
+export default Message
