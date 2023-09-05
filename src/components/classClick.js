ab04f61589a9d27ff086658375e31b91e3261d46
Benedict <newlyf38@gmail.com>
2023-09-05 04:00:55 +0000

    created a class component

---
+++ src/components/classClick.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class classClick extends Component {
+  render() {
+    return (
+      <div>classClick</div>
+    )
+  }
+}
+
+export default classClick
