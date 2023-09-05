c73eb3affe49693b00c4ddf4b71fa7da318f812c
Benedict <newlyf38@gmail.com>
2023-09-05 04:35:56 +0000

    created a component called event binding

---
+++ src/components/EventBinding.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class EventBinding extends Component {
+  render() {
+    return (
+      <div>EventBinding</div>
+    )
+  }
+}
+
+export default EventBinding
