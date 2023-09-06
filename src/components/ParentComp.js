579480f7123b1ab878006cbe85acfb19d87ca1c8
Benedict <newlyf38@gmail.com>
2023-09-06 16:32:50 +0000

    created a regular component

---
+++ src/components/ParentComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ParentComp extends Component {
+  render() {
+    return (
+      <div>ParentComp</div>
+    )
+  }
+}
+
+export default ParentComp
