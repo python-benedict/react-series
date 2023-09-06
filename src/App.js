b09504413a228ca2a52d7297fd90270e96476ad7
Benedict <newlyf38@gmail.com>
2023-09-06 18:23:59 +0000

    created a ref

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Children, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
-    <ParentComp />
+    <RefsDemo />
       {/* <Children1 heroName="Benedict" nickName="Sefa">
         <p>Hello love of my life</p>
         <div>Hello oay of my lie</div>
