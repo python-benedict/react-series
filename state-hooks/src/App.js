4a43e75a55263caf4af11c633a7cfe05e7cb0102
Benedict <newlyf38@gmail.com>
2023-09-08 19:24:14 +0000

    created an interval hook counter

--- state-hooks/src/App.js
+++ state-hooks/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import ClassMouse from './components/ClassMouse';
 import HookEffectCounterOne from './components/HookEffectCounterOne';
 import HookMouse from './components/HookMouse';
+import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      <IntervalHookCounter />
     </div>
   );
 }
