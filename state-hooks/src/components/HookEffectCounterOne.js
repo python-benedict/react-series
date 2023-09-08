ddbd040290ac3feb23333667c12b815a92b8daf2
Benedict <newlyf38@gmail.com>
2023-09-08 17:12:46 +0000

    added a conditional rendering of the useEffect to the component

--- state-hooks/src/components/HookEffectCounterOne.js
+++ state-hooks/src/components/HookEffectCounterOne.js
@@ -3,11 +3,14 @@ import React, {useState, useEffect} from 'react'
 function HookEffectCounterOne() {
 
     const [count, setCount] = useState(0)
+    const [names, setNames] = useState('')
     useEffect(()=>{
+        console.log('useEffect -- Updating')
         document.title = `You clicked ${count} times`
-    })
+    }, [count])
   return (
     <div>
+        <input type='text' value={names} onChange={e => setNames(e.target.value)}/>
         <button onClick={()=>{setCount(count + 1)}}>You clicked {count} times</button>
     </div>
   )
