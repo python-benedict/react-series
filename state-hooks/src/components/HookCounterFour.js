0f1f395926b6d2409564fa09b456416a637ca685
Benedict <newlyf38@gmail.com>
2023-09-08 10:48:24 +0000

    added a component that uses useState and arrays

--- state-hooks/src/components/HookCounterFour.js
+++ state-hooks/src/components/HookCounterFour.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function HookCounterFour() {
+    const [items, setItems] = useState([])
+
+    const addNumber = () =>{
+        setItems([...items, {
+            id: items.length,
+            value: Math.floor(Math.random()*10) + 1
+        }])
+    }
   return (
-    <div>HookCounterFour</div>
+    <div>
+        <button onClick={addNumber}>Add Number</button>
+        <ul>
+            {items.map(
+                (item) => <li key={item.id}>{item.value}</li>
+            )}
+        </ul>
+    </div>
   )
 }
 
