035ef67e7da36ae63505e8052d7c686dda9d1341
Benedict <newlyf38@gmail.com>
2023-09-08 10:13:01 +0000

    created a hookcounter three that accepts names and display them but destructure it using an array and made sure all names got don't disappear by using the ..names to keep it and JSON.stringify(names) to display the names in the object

--- state-hooks/src/components/HookCounterThree.js
+++ state-hooks/src/components/HookCounterThree.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function HookCounterThree() {
+
+    const [names, setNames] = useState({firstName: '', lastName: ''})
+
   return (
-    <div>HookCounterThree</div>
+    <div>
+        <form>
+            <input onChange={e => setNames({...names, firstName: e.target.value})} type='text' value={names.firstName} />
+            <input onChange={e => setNames({...names, lastName: e.target.value})} type='text' value={names.lastName} />
+
+            <h2>Your First Name is : {names.firstName}</h2>
+            <h2>Your Last Name is : {names.lastName}</h2>
+            <h2>{JSON.stringify(names)}</h2>
+        </form>
+    </div>
   )
 }
 
