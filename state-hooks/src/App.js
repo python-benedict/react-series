9dd4576407518a1c35e28bb0f3f3e6b90bf79340
Benedict <newlyf38@gmail.com>
2023-09-08 16:52:35 +0000

    added a useEffect hook to change the title of the document and add a counter to it

--- state-hooks/src/App.js
+++ state-hooks/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterTwo from './components/HookCounterTwo';
+import HookEffectCounterOne from './components/HookEffectCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      <HookEffectCounterOne />
     </div>
   );
 }
--- state-hooks/src/components/HookEffectCounterOne.js
+++ state-hooks/src/components/HookEffectCounterOne.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function HookEffectCounterOne() {
+
+    const [count, setCount] = useState(0)
+    useEffect(()=>{
+        document.title = `You clicked ${count} times`
+    })
   return (
-    <div>HookEffectCounterOne</div>
+    <div>
+        <button onClick={()=>{setCount(count + 1)}}>You clicked {count} times</button>
+    </div>
   )
 }
 
