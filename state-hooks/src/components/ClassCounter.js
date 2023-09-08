4ed826e6f53df17af545d142787ffbe35293ab68
Benedict <newlyf38@gmail.com>
2023-09-08 08:49:15 +0000

    created acounter component in the classcounter.js file

--- state-hooks/src/components/ClassCounter.js
+++ state-hooks/src/components/ClassCounter.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 
 export class ClassCounter extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count: 0
+      }
+    //   this.countHandler = this.countHandler.bind(this)
+    }
+
+    countHandler = () =>{
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
   render() {
+    const {count} = this.state
     return (
-      <div>ClassCounter</div>
+      <div>
+        <button onClick={this.countHandler}>Count {count}</button>
+      </div>
     )
   }
 }
