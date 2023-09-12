45e5d8316876a978f33bcd3599cb5fd89ade5f05
Benedict <newlyf38@gmail.com>
2023-09-12 18:04:09 +0000

    setup for the button component

--- state-hooks/src/components/Button.js
+++ state-hooks/src/components/Button.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function Button() {
+function Button({handleClick, children}) {
+    console.log(`Rendering button - ${children}`)
   return (
-    <div>Button</div>
+    <div>
+        <button onClick={handleClick}>
+            {children}
+        </button>
+    </div>
   )
 }
 
